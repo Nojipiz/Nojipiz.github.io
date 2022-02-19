4240fe72f562d95069f5ba3532873297b4543dbb
Orlando Vargas <nojipiz@protonmail.com>
2022-02-19 16:31:32 +0000

    Added "under construction" page

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -4,7 +4,16 @@ use yew::prelude::*;
 #[function_component(Home)]
 pub fn home() -> Html {
     html! {
+        <>
+        <a class="crab-gif"> </a>
         <div class="app">
+        <p class="tempInfoText">
+        <b>{"Website "}  {"Under Construction"} </b>
+        </p>
+        <p> {"The programmer is drinking a cup of coffee and will finish this site soon."} </p>
+        <a class="webassembly-logo"> </a>
+            <p> {"by Orlando Vargas"}</p>
         </div>
+        </>
     }
 }
