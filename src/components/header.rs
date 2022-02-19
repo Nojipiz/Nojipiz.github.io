f8711f8153950ea31137d6139463d0fbcf29e929
Orlando Vargas <nojipiz@protonmail.com>
2022-02-19 23:32:59 +0000

    Added header list

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,15 +1,48 @@
 use yew::prelude::*;
-use yew_router::prelude::*;
 
-use crate::routes::AppRoute;
+struct Section {
+    name: String,
+    section_id: String,
+}
 
 #[function_component(Header)]
 pub fn nav() -> Html {
+    let sections: [Section; 4] = [
+        Section {
+            name: String::from("Home"),
+            section_id: String::from("home"),
+        },
+        Section {
+            name: String::from("Porfolio"),
+            section_id: String::from("portfolio"),
+        },
+        Section {
+            name: String::from("About"),
+            section_id: String::from("about"),
+        },
+        Section {
+            name: String::from("Contact"),
+            section_id: String::from("contact"),
+        },
+    ];
+
+    let actual_section = use_state(|| 0);
+
+    let change_section = {
+        let actual_section = actual_section.clone();
+        Callback::from(move |_| actual_section.set(*actual_section + 1))
+    };
+
     html! {
+        <>
+        {*actual_section}
         <nav>
-            <ul>
-                <li><Link<AppRoute> to={AppRoute::Home} classes="headerRouteElement" >{ "Home" }</Link<AppRoute>></li>
-            </ul>
+        {
+            sections.iter().map(|section| {
+            html!{<ul onclick={&change_section}> {&section.name} </ul>}
+            }).collect::<Html>()
+        }
         </nav>
+        </>
     }
 }
