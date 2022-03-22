0a3c13c9c2da48044bd3a59b6b263239ab38880d
Orlando Vargas <nojipiz@protonmail.com>
2022-03-22 08:03:56 -0500

    Added text content to component

--- src/components/about.rs
+++ src/components/about.rs
@@ -6,31 +6,51 @@ use crate::{app::AppProperties, languages::languages::get_about_context_text};
 
 #[function_component(About)]
 pub fn about() -> Html {
+    let store = use_store::<BasicStore<AppProperties>>();
+    let content_text = get_about_context_text(store);
     html! {
         <section id="about" class="aboutSection">
         {"About"}
-        <WhoIAm />
+        <WhoIAm content={content_text}/>
+        <MySkills content={content_text}/>
+        <MyWork content={content_text}/>
         </section>
     }
 }
 
 #[function_component(WhoIAm)]
-fn who_i_am_component() -> Html {
-    let store = use_store::<BasicStore<AppProperties>>();
-    let content_text = get_about_context_text(store);
+fn who_i_am_component(props: &AboutProps) -> Html {
     html! {
         <>
-            <div><p>{content_text[0]} </p></div>
+            <div>
+                <h1>{props.content[0]}</h1>
+                <p>{props.content[1]}</p>
+            </div>
             <img src="https://avatarfiles.alphacoders.com/229/229959.png"/>
         </>
     }
 }
 
 #[function_component(MySkills)]
-fn my_skills() -> Html {
+fn my_skills(props: &AboutProps) -> Html {
     html! {
         <>
-        <h1>{"My Skills"}</h1>
+        <h1>{props.content[2]}</h1>
         </>
     }
+}
+
+#[function_component(MyWork)]
+fn my_work(props: &AboutProps) -> Html {
+    html! {
+        <>
+            <h1>{props.content[3]}</h1>
+            <p>{props.content[4]}</p>
+        </>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct AboutProps {
+    content: [&'static str; 5],
 }
--- src/languages/languages.rs
+++ src/languages/languages.rs
@@ -40,21 +40,37 @@ pub fn get_portfolio_content_text(store: StoreRef<BasicStore<AppProperties>>) ->
     }
 }
 
-pub fn get_about_context_text(store: StoreRef<BasicStore<AppProperties>>) -> [&'static str; 1] {
+pub fn get_about_context_text(store: StoreRef<BasicStore<AppProperties>>) -> [&'static str; 5] {
     let language: String = store
         .state()
         .map(|state| state.language.clone())
         .unwrap_or_default();
     match language.as_str() {
         "es" => [
+            "Quien soy",
             "Soy estudiante de ingenieria de sistemas y desarrollador autodidacta,
             me encanta el café, la programación competitiva, el anime y personalizar 
             sistemas linux, me apasiona escribir codigo en todas sus formas",
+            "Mis habilidades",
+            "Trabajo",
+            "Trabajo para darte el mejor codigo posible, las interfaces 
+            minimalistas, esteticas, limpias y faciles de usar son parte 
+            de mi trabajo.
+            En un proyecto, mi objetivo principal es resolver tu problema, 
+            el valor de mi trabajo es por proyecto, de esta forma me puedo 
+            concentrar en el producto sin tener que contar las horas que paso en el :)",
         ],
         _ => [
+            "Who i am",
             "I’m a systems engineering student and self-taught developer,
             I love coffee, competitive programming, anime and customizing 
             linux systems, passionate about writing code in every way.",
+            "My skills",
+            "My work",
+            "I work to give you the most flawless code possible, the beautiful,
+            clean, minimalist and easy-to-use interfaces are part of my work. In a project,
+            my main objective is to solve your problem, the value of my work is per project, 
+            so I can focus on finish it without count the hours :)",
         ],
     }
 }
