208b010561d0fbf3efe2a479a8172a2b3c3a7c15
Orlando Vargas <nojipiz@protonmail.com>
2022-03-29 22:13:59 -0500

    Added yew-hooks dependency and test for data download

--- src/components/portfolio.rs
+++ src/components/portfolio.rs
@@ -1,7 +1,8 @@
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use wasm_bindgen::JsValue;
-use wasm_bindgen_futures::spawn_local;
 use web_sys::console;
 use yew::prelude::*;
+use yew_hooks::{use_async, use_async_with_options, UseAsyncOptions};
 use yewdux::prelude::{BasicStore, PersistentStore};
 use yewdux_functional::use_store;
 
@@ -11,7 +12,6 @@ use crate::{app::AppProperties, languages::languages::get_portfolio_content_text
 pub fn portfolio() -> Html {
     let store = use_store::<PersistentStore<AppProperties>>();
     let content_text = get_portfolio_content_text(store);
-    //spawn_local(get_information());
     html! {
         <section id="portfolio" class="portfolioSection">
         <h2 class="rigthTitle"> {content_text[0]} </h2>
@@ -22,13 +22,28 @@ pub fn portfolio() -> Html {
 
 #[function_component(PortfolioElement)]
 fn get_portfolio_element() -> Html {
-    let portfolio_store = use_store::<BasicStore<PortfolioStore>>();
-    let info = portfolio_store
-        .state()
-        .map(|state| state.message.clone())
-        .unwrap_or_default();
+    let repo = use_state(|| "jetli/yew-hooks".to_string());
+    // Demo #1, manually call `run` to load data.
+    let state = {
+        let repo = repo.clone();
+        use_async(async move { fetch_repo((*repo).clone()).await })
+    };
+    let onclick = {
+        let state = state.clone();
+        Callback::from(move |_| {
+            // You can manually trigger to run in callback or use_effect.
+            state.run();
+        })
+    };
     html! {
-        <p>{info}</p>
+    <>
+        <button onclick={onclick}> {"Hi mom"}</button>
+        <p>{"element"}</p>
+        <p>{match &state.data{
+            Some(data) => &data.name,
+            None => "xd",
+        }}</p>
+    </>
     }
 }
 
@@ -43,8 +58,47 @@ async fn get_information() {
     }
 }
 
-#[derive(Clone, Default, Debug)]
-struct PortfolioStore {
-    message: String,
-    status: String,
+async fn fetch_repo(repo: String) -> Result<Repo, Error> {
+    fetch::<Repo>(format!("https://api.github.com/repos/{}", repo)).await
+}
+
+/// You can use reqwest or other crates to fetch your api.
+async fn fetch<T>(url: String) -> Result<T, Error>
+where
+    T: DeserializeOwned,
+{
+    let response = reqwest::get(url).await;
+    if let Ok(data) = response {
+        if let Ok(repo) = data.json::<T>().await {
+            Ok(repo)
+        } else {
+            Err(Error::DeserializeError)
+        }
+    } else {
+        Err(Error::RequestError)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+struct User {
+    id: i32,
+    login: String,
+    avatar_url: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+struct Repo {
+    id: i32,
+    name: String,
+    full_name: String,
+    description: String,
+    owner: User,
+}
+
+// You can use thiserror to define your errors.
+#[derive(Clone, Debug, PartialEq)]
+enum Error {
+    RequestError,
+    DeserializeError,
+    // etc.
 }
