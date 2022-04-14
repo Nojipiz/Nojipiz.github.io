04a7e4e895452c3699274d51548bebdb2468066c
Orlando Vargas <nojipiz@protonmail.com>
2022-04-14 00:00:44 -0500

    Added canvas test animation

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,5 +1,6 @@
 use crate::languages::languages::*;
-use web_sys::Window;
+use wasm_bindgen::JsCast;
+use web_sys::window;
 use yew::prelude::*;
 use yewdux::prelude::PersistentStore;
 use yewdux_functional::use_store;
@@ -11,6 +12,7 @@ pub fn home() -> Html {
     html! {
         <section id="home" class="homeSection">
             <PresentationTitle />
+            <canvas id={"uniqueElement"}> </canvas>
             <DonutAnimation />
         </section>
     }
@@ -19,7 +21,6 @@ pub fn home() -> Html {
 #[function_component(DonutAnimation)]
 fn donut_animation() -> Html {
     html! {
-        //Todo: Generate donut code in rust
         <Donut/>
     }
 }
@@ -39,8 +40,42 @@ fn presentation_title() -> Html {
 
 #[function_component(Donut)]
 fn donnut_code() -> Html {
-    let win: Window = web_sys::window().unwrap();
-    let res = win.set_interval_with_str_and_timeout_and_unused_0("console.log('Hello')", 1000);
-    res.unwrap();
+    let win = web_sys::window().unwrap();
+    let document = win.document().unwrap();
+    let canvas: web_sys::HtmlCanvasElement = document
+        .get_element_by_id("uniqueElement")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+    // create a counter animation in the canvas that updates every second
+    let ctx = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+    let mut counter = 0;
+    let mut interval = win.set_interval_with_callback_and_timeout_and_arguments_0(
+        move || {
+            counter += 1;
+            ctx.clear_rect(0.0, 0.0, canvas.width(), canvas.height());
+            ctx.begin_path();
+            ctx.arc(
+                canvas.width() / 2.0,
+                canvas.height() / 2.0,
+                canvas.width() / 2.0,
+                0.0,
+                2.0 * std::f32::consts::PI,
+            );
+            ctx.stroke();
+            ctx.fill_style = "white".into();
+            ctx.fill_text(
+                &format!("{}", counter),
+                canvas.width() / 2.0,
+                canvas.height() / 2.0,
+            );
+        },
+        1000,
+    );
     html! {}
 }
