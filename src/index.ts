c6d30a5fe9351adc976831eef7c0f34ca5a6d1e5
Orlando Vargas <nojipiz@protonmail.com>
2022-04-27 17:57:44 -0500

    Structure made for scss styles por component

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,12 @@
 // @ts-ignore: Typescript don't know about Cargo.toml import run but it does
-import { run } from '../Cargo.toml'
-import * as BABYLON from 'babylonjs'
-run()
+import { run } from '../Cargo.toml';
+import startDonutAnimation from './animations/donutAnimation';
 
-console.log("Hi mom");
+//This is the main function that will load the wasm data into the browser
+// Main Function
+run();
 
+// Animations //
+startDonutAnimation();
 
-// Lets create a babylonjs animation to the donut canvas
-const donutCanva: HTMLCanvasElement = document.getElementById("donutCanva") as HTMLCanvasElement;
-const engine = new BABYLON.Engine(donutCanva, true);
-
-function createScene(): BABYLON.Scene {
-  const scene = new BABYLON.Scene(engine);
-  const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, -5), scene);
-  camera.attachControl(donutCanva, true);
-  const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
-  const box = BABYLON.MeshBuilder.CreateBox('box', { size: 1 }, scene);
-  return scene
-}
-
-const scene = createScene();
-
-engine.runRenderLoop(() => {
-  scene.render();
-})
 
