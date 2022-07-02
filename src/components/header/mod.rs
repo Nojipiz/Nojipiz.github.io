8350e92f489399c36a90b2637b81e65fac5983a6
Orlando Vargas <nojipiz@protonmail.com>
2022-07-01 22:21:12 -0500

    Added simple icons to buttons

--- src/components/header/mod.rs
+++ src/components/header/mod.rs
@@ -53,8 +53,14 @@ fn get_properties_buttons() -> Html {
     });
     html! {
         <div class={"options_wrapper"}>
-            <button onclick={change_language}>{language}</button>
-            <button onclick={change_color_scheme}>{color_scheme}</button>
+            <button onclick={change_language}>
+                <LanguageIcon/>
+                {language}
+            </button>
+            <button onclick={change_color_scheme}>
+                <ColorSchemeIcon/>
+                {color_scheme}
+            </button>
         </div>
     }
 }
@@ -103,4 +109,24 @@ fn update_body_class(color_scheme: &str) {
         "dark" => "theme_dark",
         _ => "",
     })
+}
+
+#[function_component(ColorSchemeIcon)]
+fn color_scheme_icon() -> Html {
+    html! {
+    <svg width="16" height="16" fill="currentColor" class="bi bi-palette" viewBox="0 0 16 16">
+      <path d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+      <path d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8zm-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7z"/>
+    </svg>
+    }
+}
+
+#[function_component(LanguageIcon)]
+fn language_icon() -> Html {
+    html! {
+    <svg width="16" height="16" fill="currentColor" class="bi bi-translate" viewBox="0 0 16 16">
+      <path d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286H4.545zm1.634-.736L5.5 3.956h-.049l-.679 2.022H6.18z"/>
+      <path d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2zm7.138 9.995c.193.301.402.583.63.846-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6.066 6.066 0 0 1-.415-.492 1.988 1.988 0 0 1-.94.31z"/>
+    </svg>
+    }
 }
